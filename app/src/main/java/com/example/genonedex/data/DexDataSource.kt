e1b4968dd4437aed6104fb8809dca133182f2005
AS-Coope <deltazirconium@gmail.com>
2023-07-22 09:22:13 -0500

    Introduce DataSource and DexEntry classes for data format

---
+++ app/src/main/java/com/example/genonedex/data/DexDataSource.kt
@@ -0,0 +1,14 @@
+package com.example.genonedex.data
+
+class DexDataSource {
+
+    // formats the data to be presented on the Recycler View
+    fun loadScrollEntry(){
+
+    }
+
+    // formats the data to be presented in the description entry page
+    fun loadDescEntry(){
+
+    }
+}
---
+++ app/src/main/java/com/example/genonedex/model/DexEntry.kt
@@ -0,0 +1,14 @@
+package com.example.genonedex.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class DexEntry(
+    @StringRes private val dexNo: Int,
+    @DrawableRes private val miniSprite: Int,
+    @DrawableRes private val regSprite: Int,
+    @StringRes private val name: Int,
+    @StringRes private val desc: Int,
+    @StringRes private val weight: Int,
+    @StringRes private val height: Int,
+)
