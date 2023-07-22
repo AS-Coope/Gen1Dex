84982b27016caabe2abfee28f205330da3f3fa95
AS-Coope <deltazirconium@gmail.com>
2023-07-22 14:14:22 -0500

    Setup viewbinding and linearlayout recycler view in MainActivity.kt

--- app/src/main/java/com/example/genonedex/MainActivity.kt
+++ app/src/main/java/com/example/genonedex/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.genonedex
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.genonedex.adapter.DexAdapter
+import com.example.genonedex.data.DexDataSource
+import com.example.genonedex.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val dexScrollEntries = DexDataSource().loadScrollEntry()
+
+        binding.mainRecyclerView.layoutManager = LinearLayoutManager(this)
+        binding.mainRecyclerView.adapter = DexAdapter(dexScrollEntries)
+        binding.mainRecyclerView.setHasFixedSize(true)
+
     }
 }
