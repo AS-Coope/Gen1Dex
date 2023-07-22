496f71d5d77f34fbf8c19366564d083365846e8f
AS-Coope <deltazirconium@gmail.com>
2023-07-22 14:06:45 -0500

    Add Dex 1 Entry to listOf in DexDataSource

--- app/src/main/java/com/example/genonedex/data/DexDataSource.kt
+++ app/src/main/java/com/example/genonedex/data/DexDataSource.kt
@@ -1,12 +1,15 @@
 package com.example.genonedex.data
 
+import com.example.genonedex.R
 import com.example.genonedex.model.DexScrollEntry
 
 class DexDataSource {
 
     // formats the data to be presented on the Recycler View
-    fun loadScrollEntry(){
-
+    fun loadScrollEntry(): List<DexScrollEntry>{
+        return listOf(
+            DexScrollEntry(R.string.bulbasaur_dex_no, R.drawable.bulbasaur_item_sprite, R.string.bulbasaur_name)
+        )
     }
 
     // formats the data to be presented in the description entry page
