c77f14e8cc0f3d820c4cf9bab85d85bfd559baa3
AS-Coope <deltazirconium@gmail.com>
2023-08-04 23:00:23 -0500

    Add Dex Entries 2 - 9 in loadDexEntry function

--- app/src/main/java/com/example/genonedex/data/DexDataSource.kt
+++ app/src/main/java/com/example/genonedex/data/DexDataSource.kt
@@ -51,8 +51,36 @@ class DexDataSource {
     // formats the data to be presented in the description entry page
     fun loadDescEntry(): List<DexEntry>{
         return listOf(
+            ////////// 1 - 3
             DexEntry(
-                R.string.bulbasaur_dex_no, R.drawable.bulbasaur_item_sprite, R.string.bulbasaur_name, R.string.bulbasaur_desc, R.string.bulbasaur_weight, R.string.bulbasaur_height, R.string.type_grass, R.string.type_poison)
+                R.string.bulbasaur_dex_no, R.drawable.bulbasaur_item_sprite, R.string.bulbasaur_name, R.string.bulbasaur_species,R.string.bulbasaur_desc, R.string.bulbasaur_weight, R.string.bulbasaur_height, R.string.type_grass, R.string.type_poison
+            ),
+            DexEntry(
+                R.string.ivysaur_dex_no, R.drawable.ivysaur_item_sprite, R.string.ivysaur_name, R.string.ivysaur_species,R.string.ivysaur_desc, R.string.ivysaur_weight, R.string.ivysaur_height, R.string.type_grass, R.string.type_poison
+            ),
+            DexEntry(
+                R.string.venusaur_dex_no, R.drawable.venusaur_item_sprite, R.string.venusaur_name, R.string.venusaur_species,R.string.venusaur_desc, R.string.venusaur_weight, R.string.venusaur_height, R.string.type_grass, R.string.type_poison
+            ),
+            ////////// 4 - 6
+            DexEntry(
+                R.string.charmander_dex_no, R.drawable.charmander_item_view, R.string.charmander_name, R.string.charmander_species,R.string.charmander_desc, R.string.charmander_weight, R.string.charmander_height, R.string.type_fire, R.string.no_type_2
+            ),
+            DexEntry(
+                R.string.charmeleon_dex_no, R.drawable.charmeleon_item_view, R.string.charmeleon_name, R.string.charmeleon_species,R.string.charmeleon_desc, R.string.charmeleon_weight, R.string.charmeleon_height, R.string.type_fire, R.string.no_type_2
+            ),
+            DexEntry(
+                R.string.charizard_dex_no, R.drawable.charizard_item_view, R.string.charizard_name, R.string.charizard_species,R.string.charizard_desc, R.string.charizard_weight, R.string.charizard_height, R.string.type_fire, R.string.type_flying
+            ),
+            /////////// 7 - 9
+            DexEntry(
+                R.string.squirtle_dex_no, R.drawable.squirtle_item_view, R.string.squirtle_name, R.string.squirtle_species,R.string.squirtle_desc, R.string.squirtle_weight, R.string.squirtle_height, R.string.type_water, R.string.no_type_2
+            ),
+            DexEntry(
+                R.string.wartortle_dex_no, R.drawable.wartortle_image_view, R.string.wartortle_name, R.string.wartortle_species,R.string.wartortle_desc, R.string.wartortle_weight, R.string.wartortle_height, R.string.type_water, R.string.no_type_2
+            ),
+            DexEntry(
+                R.string.blastoise_dex_no, R.drawable.blastoise_item_view, R.string.blastoise_name, R.string.blastoise_species,R.string.blastoise_desc, R.string.blastoise_weight, R.string.blastoise_height, R.string.type_water, R.string.no_type_2
+            ),
         )
     }
 }
