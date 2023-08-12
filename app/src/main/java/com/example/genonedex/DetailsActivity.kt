2f9cb19070dc90886580bec389f150800b07b04f
AS-Coope <deltazirconium@gmail.com>
2023-08-12 11:29:18 -0500

    Achieve successful loading of data from list to detailsActivity

--- app/src/main/java/com/example/genonedex/DetailsActivity.kt
+++ app/src/main/java/com/example/genonedex/DetailsActivity.kt
@@ -8,18 +8,26 @@ import com.example.genonedex.databinding.ActivityDetailsBinding
 class DetailsActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityDetailsBinding
+    private val dexScrollEntries = DexDataSource().loadDescEntry()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDetailsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val dexEntryId = intent.getStringExtra("DEX_ENTRY")
-        val dexScrollEntries = DexDataSource().loadDescEntry()
+        val dexEntryId = intent.getIntExtra("DEX_ENTRY", -1)
 
-        val dexEntryDetails = fetchDexEntry(dexEntryId)
+        fetchDexEntry(dexEntryId)
+
     }
 
-    private fun fetchDexEntry(dexEntryId: String?) {
+    private fun fetchDexEntry(dexEntryId: Int) {
+        binding.detailsDexImage.setImageResource(dexScrollEntries[dexEntryId].regSprite)
+        binding.dexEntryName.setText(dexScrollEntries[dexEntryId].name)
+        binding.dexEntrySpecies.setText(dexScrollEntries[dexEntryId].dexSpecies)
+        binding.dexEntryType.setText(dexScrollEntries[dexEntryId].type1)
+        binding.dexEntryWeight.setText(dexScrollEntries[dexEntryId].weight)
+        binding.dexEntryHeight.setText(dexScrollEntries[dexEntryId].height)
+        binding.dexEntryDetails.setText(dexScrollEntries[dexEntryId].desc)
 
     }
 }
