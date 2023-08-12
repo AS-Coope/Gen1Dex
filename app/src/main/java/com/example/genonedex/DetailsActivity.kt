7e010b7090895940811a31d1a7cc567ab14090d0
AS-Coope <deltazirconium@gmail.com>
2023-08-12 10:20:33 -0500

    Fix render problem in layout; add view binding variable to DetailsActivity

--- app/src/main/java/com/example/genonedex/DetailsActivity.kt
+++ app/src/main/java/com/example/genonedex/DetailsActivity.kt
@@ -2,10 +2,14 @@ package com.example.genonedex
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.genonedex.databinding.ActivityDetailsBinding
 
 class DetailsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetailsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_details)
+        binding = ActivityDetailsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
